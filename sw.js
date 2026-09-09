d90e7d5ecc86451e24e4034eb08a9061ce5a8734
sm960826-design <sm960826@gmail.com>
2026-09-10 08:47:58 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-const C='attend-v9';
+const C='attend-v10';
 const FILES=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png',
- 'https://cdnjs.cloudflare.com/ajax/libs/xlsx/0.18.5/xlsx.full.min.js',
+ 'https://cdnjs.cloudflare.com/ajax/libs/xlsx/0.18.5/xlsx.full.min.js','https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.9/dist/web/static/pretendard.min.css',
  'https://fonts.googleapis.com/css2?family=Nanum+Pen+Script&display=swap','https://cdn.jsdelivr.net/gh/webfontworld/kopub/KoPubWorldDotum.css'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(C).then(c=>Promise.allSettled(FILES.map(f=>c.add(f)))).then(()=>self.skipWaiting()));});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==C).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
